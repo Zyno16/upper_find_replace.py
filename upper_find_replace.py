e0c4f96757f2aa4f981b07775d37b2df704814eb
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-11-07 19:20:05 +0000

    Add files via upload

---
+++ upper_find_replace.py
@@ -0,0 +1,14 @@
+
+#this to display the position of any letter and if you have the result is -1 this mean the letter in not fount
+fruit = "banana"
+letter = fruit.find("na")
+print (letter)
+x =fruit.find("zw")
+print (x)
+#upper case
+veg = "potato"
+y = veg.upper()
+print(y)
+rep = veg.replace("t","b")
+print(rep)
+
